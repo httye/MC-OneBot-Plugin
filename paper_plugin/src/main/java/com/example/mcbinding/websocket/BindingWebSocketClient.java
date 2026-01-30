83359303adf7816262a2511d2945a9dfe10249ee
httye <1280773070@qq.com>
2026-01-30 20:00:01 +0800

    修改ws

--- paper_plugin/src/main/java/com/example/mcbinding/websocket/BindingWebSocketClient.java
+++ paper_plugin/src/main/java/com/example/mcbinding/websocket/BindingWebSocketClient.java
@@ -4,7 +4,7 @@ import com.example.mcbinding.MCBindingPlugin;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 import org.java_websocket.exceptions.WebsocketNotConnectedException;
-import org.java_websocket.WebSocket.READYSTATE;
+import org.java_websocket.WebSocket;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import java.net.URI;
@@ -57,8 +57,8 @@ public class BindingWebSocketClient extends WebSocketClient {
     }
     
     public boolean isConnected() {
-        // 修复READYSTATE问题，使用正确的READYSTATE枚举
-        return connected && this.getReadyState() == READYSTATE.OPEN;
+        // 修复READYSTATE问题，使用getReadyState()方法直接比较
+        return connected && this.getReadyState() == WebSocket.READYSTATE.OPEN;
     }
     
     public void sendBindingSuccess(String qqId, String minecraftName) {
